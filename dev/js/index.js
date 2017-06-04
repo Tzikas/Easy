d07f77895ad6d176358354e78a4f5ea88ab9398d
squirrl <niko.tzikas@gmail.com>
2017-06-04 18:13:08 -0400

    foood

--- dev/js/index.js
+++ dev/js/index.js
@@ -4,74 +4,132 @@ import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
 // React component
-class Whatever extends Component {
+class anyComponent extends Component {
 
-	render() {
-		const { superDuperComplicatedFunction } = this.props
+	createList() {
+		console.log('in create list')
+		console.log(this)
+		return this.props.state.foods.map((food, i) => {
+			console.log(food)
+			return ( 
+				<li key={i}> {food} </li>					
+			)
+		})
+	}
+	
+	render() {	
 			return (
-					<div>
-						<button onClick={function() { alert('this is a simple inline function'); console.log(this) } }>Simple Inline Function</button>  						
-						<button onClick={superDuperComplicatedFunction}>Super Duper Complicated Function </button>
+					<div onClick = {this.props.thankyou}>						
+						<button onClick={ this.props.breakfast }>Are you serving breakfast?</button> 							    			  	     <button onClick={ this.props.lunch }>Are you serving lunch?</button> 						          		      	                  <button onClick={ this.props.dinner }>Are you serving dinner?</button> 									
+						<ul>
+
+							{ this.createList() }
+
+						</ul>	
 					</div>
+
 			       )
 	}
 }
 
 
+
+
+function foodTimes(dispatch) {
+	console.log('dispatch:');console.log(dispatch);
+	return {
+		dinner: function(){
+			alert(' we are not serving dinner' ); 			
+		},
+		lunch: function(){
+			alert(' we are serving lunch' ); 			
+
+			dispatch(lunchAction); 
+			
+		},
+		breakfast: function(){
+			alert(' we are not serving breakfast' ); 			
+		}
+	       
+	}
+}
+
+function thanks(dispatch){
+	return { 
+		thankyou: function(){ 
+			alert('ok thanks')
+		}
+		
+	}
+}
+
 const define_an_action = {
 		type:'use this to finally call the action that will change the state',
 		whatever: {'you':['want',2,'put']},
-		whatIsThis: function(){ console.log(this); }
-	        	
+		whatIsThis: function(){ console.log(this); }	        	
 }
+
+
+const lunchAction = {
+		type:'LUNCH TIME',
+		lunchFoods: ['kale soup', 'grilled cheese'] 
+		
+
+} 
+
+
 
 
 
 // Reducer
-function REDUCER__________useThisToChangeTheStuffWithinTheStore(state={blah:'blah'}, action){
-	        console.log(action) 
+function addStuffToStore(state={foods:['mango']}, action){
+  		console.log('action:');console.log(action)
+		console.log('state:');console.log(state);
+		//if(action.lunchFoods){
+		//	let newState = [...state.foods, ...action.lunchFoods]
+		//	console.log(newState);
+				
+		//}
 		switch (action.type) {
-			case 'use this to finally call the action that will change the state':
-				action.whatIsThis(); 
-				action.whatever = 234567890;
-				return state.blah = 'nindfsdko';
+			case 'LUNCH TIME':
+				if(action.lunchFoods){
+					state.foods = [...state.foods, ...action.lunchFoods];
+					console.log(state);
+					return state;
+				}
+					
 			default:
 				return state
 
 		}
 
 }
-const store = createStore(REDUCER__________useThisToChangeTheStuffWithinTheStore) 
 
 
-function mapStateToProps(state) {
-		console.log(state)
-		return {
-			state
-		}
-}
+
+const foodStore = createStore(addStuffToStore) 
 
 
-function mapDispatchToProps(dispatch) {
+function food(state){
+	console.log('food')	
+	console.log(state);
+	//state.foods = stuff;
 	return {
-		superDuperComplicatedFunction: function(){
-			alert('superDuperCompldddicatedFunction')
-			console.log(this);
-			dispatch(define_an_action)
-		}  
-
+		state
 	}
+	
 }
+
 
 // Connected Component
 const App = connect(
-		mapStateToProps,
-		mapDispatchToProps
-		)(Whatever)
+		foodTimes,		
+		food	
+		)(anyComponent)
 
 ReactDOM.render(
-		<Provider store={store}>
-		<App />
+		<Provider store={foodStore}>
+			<App/> 
 		</Provider>,
 		document.getElementById('root')
 	       )
---
+++ dev/js/indexOld.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+// React component
+class Whatever extends Component {
+
+	render() {
+		const { superDuperComplicatedFunction } = this.props
+			return (
+					<div>
+						<button onClick={function() { alert('this is a simple inline function'); console.log(this) } }>Simple Inline Function</button>  						
+						<button onClick={superDuperComplicatedFunction}>Super Duper Complicated Function </button>
+					</div>
+			       )
+	}
+}
+
+
+const define_an_action = {
+		type:'use this to finally call the action that will change the state',
+		whatever: {'you':['want',2,'put']},
+		whatIsThis: function(){ console.log(this); }
+	        	
+}
+
+
+
+// Reducer
+function REDUCER__________useThisToChangeTheStuffWithinTheStore(state={blah:'blah'}, action){
+	        console.log(action) 
+		switch (action.type) {
+			case 'use this to finally call the action that will change the state':
+				action.whatIsThis(); 
+				action.whatever = 234567890;
+				return state.blah = 'nindfsdko';
+			default:
+				return state
+
+		}
+
+}
+const store = createStore(REDUCER__________useThisToChangeTheStuffWithinTheStore) 
+
+
+function mapStateToProps(state) {
+		console.log(state)
+		return {
+			state
+		}
+}
+
+
+function mapDispatchToProps(dispatch) {
+	return {
+		superDuperComplicatedFunction: function(){
+			alert('superDuperCompldddicatedFunction')
+			console.log(this);
+			dispatch(define_an_action)
+		}  
+
+	}
+}
+
+// Connected Component
+const App = connect(
+		mapStateToProps,
+		mapDispatchToProps
+		)(Whatever)
+
+ReactDOM.render(
+		<Provider store={store}>
+		<App />
+		</Provider>,
+		document.getElementById('root')
+	       )
+
+
