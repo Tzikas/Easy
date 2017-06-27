55bd03d15f52835adf63c0da628fc8b4a353c34e
squirrl <niko.tzikas@gmail.com>
2017-06-27 16:44:42 -0400

    asdf

--- dev/js/index.js
+++ dev/js/index.js
@@ -4,154 +4,238 @@ import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
 // React component
-class anyComponent extends Component {
-	constructor(props) {
-		super(props);
+class whateverClass extends Component {
+ 	constructor (props, context) {
+    		super(props, context)
+		console.log(this, props, context)
+    		this.state = {
+      			counter: 0
+    		}
+		this.add = this.add.bind(this);
+		this.subtract = this.subtract.bind(this);
+		
+		//this.props.insideAction = this.props.insideAction.bind(this);
+	}
 
-		this.state = {
-			foods: []
-		};
-		this.setLunch = this.setLunch.bind(this);
+	add(){
+		//console.log('insideClass ',this) 							
+		//console.log(store.getState())			
+		this.state.counter++; 
+		this.forceUpdate()
+
+		//this.setState({counter:this.state.counter++})
+	}
+	subtract(){
+		//console.log('insideClass ',this) 							
+		//console.log(store.getState())			
+		this.state.counter--; 
+		this.forceUpdate()
+
+		//this.setState({counter:this.state.counter++})
+	}
+	
+	update(e){
+		this.setState({
+			a:this.refs.a.value,
+			b:e.target.value
+		})
+	}
+	
+	render() {
+
+		    	var style = {
+      				color: this.props.current ,
+				fontSize:this.state.counter + 'em'
+	  	  	};
+			return (
+					<div>
+						
+						<hr />
+						<p>inside render</p>
+						
+						<input ref="a" type="text" onChange={this.update.bind(this)} />{ this.state.a }
+						<br />
+						
+						<input ref="b" type="text" onChange={this.update.bind(this)} />{ this.state.b }
+
+						<br />
+						<button ref={this.state.counter} onClick={function() { 
+							
+ 							console.log('inside button', this); 
+							console.log(store.getState())
+							//console.log({this.state.counter})
+							this.state.counter--; 
+							this.forceUpdate()
+
+						
+						} }>insideButton {this.state.counter} </button>
+
+
+
+						<hr />
+						<p>inside class</p>
+
+						<button onClick={this.add}> + </button>
+						<button onClick={this.subtract}> - </button>
+
+
+
+						<hr />
+						<p>inside props, actions & reducers</p>
+						
+						<button onClick={this.props.insideProps}>insideProps </button>	
+
+						<button onClick={this.props.insideAction}>insideAction </button>	
+
+						<button onClick={this.props.insideReducer}>insideReducer </button>
+
+						<h1 style={style}>{this.state.counter}</h1>
+						<h2>{this.props.current}</h2>
+						<h2 style={style} >{this.state.a}</h2>
+						<Welcome /> 
+					
+						<newComponent/>
+					</div>
+			       )
+	}
+}
+
+
+
+
+
+class newComponent extends Component {
+ 	constructor (props, context) {
+    		super(props, context)
+		console.log(this, props, context)
+	}
+	render() {
+		return(
+			<div>sup</div>	
+		      )
+	}	
+
+}
+
+class Welcome extends React.Component {
+ 	constructor (props, context) {
+    		super(props, context)
+		console.log(this, props, context)
+	}
+	
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+
+//first
+function mapDispatchToProps(dispatch) {
+	return {
+		insideProps:function(props){
+			//that.state.counter++; 
+			//that.forceUpdate();
+			
+			
+			console.log('%c inside Props','background:yellow');
+			console.log(props)
+			var d1 = document.getElementById('root');
+			d1.insertAdjacentHTML('beforeend', <App />);			
+				
+			//if(document.body != null){ document.body.append('<p>sup</p>') } 
+		},
+		insideReducer:function(){
+			dispatch(defineAction); 
+		},
+		insideAction:function(){
+			dispatch(defineAnotherAction); 
+		} 
+		
+	}
+}
+
+
+const defineAction = {
+	type:'call your action'
+}
+
+
+
+const defineAnotherAction = {
+	type:'call another action',
+	another: function(){
+		console.log('%c insideAction ','background: red; color: #bada55'); 
+	}
+}
+
+
+function reducer(state={current:'nada'}, action){
+	//console.log('- called reducer ',state,action) 
+	switch (action.type) {
+		case 'call your action':
+			console.log('%c insideReducer', 'background: blue; color: #bada55')
+			return state={'current':'blue'}
+			//return this.setState({'current':'blue'})
+
+		case 'call another action':
+			action.another()
+			//return this.setState({'current':'red'})	
+			return state={'current':'red'}			
+		default:
+  			return state;
+	}
+}
+	
 		
 
-	}
+const store = createStore(reducer) 
 
 
-	createList() {
-		console.log('in create list')
-		console.log(this)
-			return this.state.foods.map((food, i) => {
-				console.log(food)
-				return ( 
-					<li key={i}> {food} </li>					
-				       )
-			})
-	}
-
-	render() {	
-		return (
-				<div onClick = {this.props.thankyou}>						
-					<button onClick={ this.props.breakfast }>Are you serving breakfast?</button> 							    			  		  <button onClick={ this.props.lunch }>Are you serving lunch?</button> 	
-					<button onClick={ this.setLunch }>Are you settingLunch?</button> 					
-					<button onClick={ this.props.dinner }>Are you serving dinner?</button> 									
-					<ul>
-
-						{ this.createList() }
-
-					</ul>	
-				</div>
-
-		       )
-	}
-
-	setLunch(dispatch){
-		alert(' we are serving lunch' ); 			
-		console.log(this)
-		//this.setState({'foods':this.state.foods})
-			
-		this.setState({'foods':['a','b']})
-	}
-}
-
-
-
-
-function foodTimes(dispatch) {
-	console.log('dispatch:');console.log(dispatch);
-	return {
-		dinner: function(){
-			alert(' we are not serving dinner' ); 			
-		},
-			lunch: function(){
-				alert(' we are serving lunch' ); 			
-
-				dispatch(lunchAction); 
-
-			},
-			breakfast: function(){
-				alert(' we are not serving breakfast' ); 			
-			}
-
-	}
-}
-
-function thanks(dispatch){
-	return { 
-		thankyou: function(){ 
-			alert('ok thanks')
+function mapStateToProps(state) {
+		///console.log('- called map state to props ',state)
+		return {
+	//		state
+ 	 	  	current: state.current
+	//		
 		}
-
-	}
-}
-
-const define_an_action = {
-	type:'use this to finally call the action that will change the state',
-	whatever: {'you':['want',2,'put']},
-	whatIsThis: function(){ console.log(this); }	        	
 }
 
 
-const lunchAction = {
-	type:'LUNCH TIME',
-	lunchFoods: ['kale soup', 'grilled cheese'] 
-
-
-} 
-
-
-
-
-
-// Reducer
-function addStuffToStore(state={foods:['mango']}, action){
-	console.log('action:');console.log(action)
-	console.log('state:');console.log(state);
-	console.log(this)
-	switch (action.type) {
-		case 'LUNCH TIME':
-			if(action.lunchFoods){
-				state.foods = [...state.foods, ...action.lunchFoods]
-				console.log(state);
-
-				return state;
-			}
-
-		default:
-			return state
-
-	}
-
-}
-
-
-
-const foodStore = createStore(addStuffToStore) 
-
-
-function food(state){
-	console.log('food')	
-	console.log(state);
-	//state.foods = stuff;
-	return {
-		state
-	}
-
-}
 
 
 // Connected Component
 const App = connect(
-		food,
-		foodTimes
-
-		)(anyComponent)
-
+		mapStateToProps,
+		mapDispatchToProps
+		)(whateverClass)
 
 ReactDOM.render(
-		<Provider store={foodStore}>
-		<App/> 
+		<Provider store={store}>
+			<App />
 		</Provider>,
 		document.getElementById('root')
 	       )
+
+
+
+
+
+
+/*
+	componentWillMount() {
+		console.log('componentWillMount')
+	}
+	componentDidMount() {
+		console.log('componentDidMount')
+	}
+	componentDidUpdate(prevProps, prevState){
+		console.log('componentDidUpdate')
+	}
+	componentWillUpdate(prevProps, prevState){
+		console.log('componentWillUpdate')
+	}
+	componentWillReceiveProps(nextProps) {
+		console.log('componentWillReceiveProps')
+	}*/
 
 
